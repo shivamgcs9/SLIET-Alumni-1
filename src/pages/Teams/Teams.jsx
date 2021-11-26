637a36f04123437d04744720f7d8ee2993b64ea6
Shivam Kumar <shivamgcs9@gmail.com>
2021-11-26 16:38:27 +0530

    shivam commit added filters

--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -1,16 +1,20 @@
+import { FaSearch } from "react-icons/fa";
 import styled from "styled-components";
+import Filter from "../../components/Team/Filters/Filters";
 import Students from "../../components/Team/students/Students";
 
 const Container = styled.div`
   width: 100%;
   height: calc(100vh- 80px);
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  /* align-items: center; */
 `;
 
 const Teams = () => {
   return (
     <Container>
+      <Filter />
       <Students />
     </Container>
   );
