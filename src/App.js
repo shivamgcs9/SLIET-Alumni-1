795ada8559677fadadc4640e821f02f544415f73
Harsiman Singh <harsimran.sliet@gmail.com>
2021-11-26 19:15:30 +0530

    faq

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import About from "./pages/About/About";
 import Blog from "./pages/Blog/Blog";
 import Contact from "./pages/Contact/Contact";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Faq from "./pages/Faq/Faq";
+import Faq from "./components/Faq/faq";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
