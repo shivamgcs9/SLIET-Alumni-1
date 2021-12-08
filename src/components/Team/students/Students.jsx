b7600cd96ca31fb9b303385a12a08bba16e53f14
Shivam Kumar <shivamgcs9@gmail.com>
2021-12-08 14:36:22 +0530

    commit

--- src/components/Team/students/Students.jsx
+++ src/components/Team/students/Students.jsx
@@ -65,104 +65,6 @@ const Button = styled.button`
   margin-top: 10px;
 `;
 
-// const ContainerFill = styled.div`
-//   /* margin: 18px 200px; */
-//   width: 100%;
-
-//   margin: auto;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: space-between;
-
-//   @media screen and (max-width: 768px) {
-//     margin: 18px;
-//     flex-direction: column;
-//   }
-// `;
-
-// const Filter = styled.div`
-//   margin-top: 20px;
-//   flex: 1.5;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   @media screen and (max-width: 768px) {
-//   }
-// `;
-// const MobileFilter = styled.div`
-//   flex: 1.5;
-//   flex-wrap: wrap;
-//   align-items: center;
-//   justify-content: space-between;
-//   display: none;
-//   @media screen and (max-width: 768px) {
-//     flex-direction: column;
-//     display: flex;
-//     position: absolute;
-//     background-color: lightskyblue;
-//     bottom: 0;
-//     z-index: 999;
-//     display: none;
-//   }
-// `;
-
-// const FilterItem = styled.div`
-//   @media screen and (max-width: 768px) {
-//     width: 300px;
-//     margin-bottom: 1rem;
-//   }
-// `;
-
-// const Span = styled.span`
-//   font-weight: bold;
-//   /* margin: 0 15px 0 45px; */
-//   cursor: pointer;
-//   display: none;
-//   @media screen and (max-width: 768px) {
-//   }
-// `;
-
-// const Select = styled.select`
-//   padding: 10px;
-//   margin-left: 20px;
-//   border: 1px solid rgb(90, 180, 220);
-//   border-radius: 5px;
-//   @media screen and (max-width: 768px) {
-//     width: 100%;
-//     margin: auto;
-//   }
-// `;
-// const Option = styled.option``;
-
-// const SearchContainer = styled.div`
-//   flex: 1;
-//   @media screen and (max-width: 768px) {
-//     margin-bottom: 20px;
-//   }
-// `;
-
-// const InputBox = styled.div`
-//   padding: 10px;
-//   border: 1px solid rgb(90, 180, 220);
-//   border-radius: 10px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-// `;
-
-// const Input = styled.input`
-//   width: 100%;
-//   border: none;
-//   outline: none;
-// `;
-
-// const Button = styled.button`
-//   display: none;
-//   @media screen and (max-width: 768px) {
-//     display: none;
-//   }
-// `;
-
 const Container = styled.div`
   padding: 0 100px;
   display: flex;
@@ -289,49 +191,10 @@ const Students = () => {
                     <Option value="Shimla">Shimla</Option>
                   </Select>
                 </FilterItem>
+                <Button onClick={() => setOpen(!open)}>Apply</Button>
               </>
             )}
-            <Button onClick={() => setOpen(!open)}>Apply</Button>
           </Filter>
-          <MobileFilter>
-            <FilterItem>
-              <Span onClick={() => setOpen(!open)}>Filters:</Span>
-            </FilterItem>
-
-            {open && (
-              <>
-                <FilterItem>
-                  <Select>
-                    <Option defaultValue>course</Option>
-                    <Option>Computer Science</Option>
-                    <Option>Mechanical Engineering</Option>
-                    <Option>Chemical Engineering</Option>
-                    <Option>Food Engineering</Option>
-                  </Select>
-                </FilterItem>
-                <FilterItem className="filterItem">
-                  <Select name="" id="">
-                    <Option defaultValue>Graduation Year</Option>
-                    <Option value="">2018</Option>
-                    <Option value="">2019</Option>
-                    <Option value="">2020</Option>
-                    <Option value="">2021</Option>
-                    <Option value="">2022</Option>
-                  </Select>
-                </FilterItem>
-                <FilterItem>
-                  <Select name="" id="">
-                    <Option defaultValue>Location</Option>
-                    <Option value="">Uttrakhand</Option>
-                    <Option value="">Punjab</Option>
-                    <Option value="">Himachal</Option>
-                    <Option value="">Delhi</Option>
-                    <Option value="">Shimla</Option>
-                  </Select>
-                </FilterItem>
-              </>
-            )}
-          </MobileFilter>
         </ContainerFill>
         <Container>{displayUsers}</Container>
         {isLoaded ? (
